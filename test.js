1e8adc1df7b13b687233a4a6375f3778adad4fb1
TyoeOracle <typeoracle@protonmail.com>
2022-01-17 22:37:02 +0800

    message

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var media_id="gh_77aef1d9cf29";
 var storage_item = "ui3_" + media_id;
-alert("HI");
+alert("HI2");
 alert(location.href);
 alert(localStorage.getItem(storage_item));	
 var iframe=document.createElement("iframe");
